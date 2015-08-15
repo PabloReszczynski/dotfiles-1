70bd8bde771cb5d498de9cd863a64ea4081e75e1
braph <braph@users.noreply.github.com>
2015-08-15 20:18:32 +0200

    edited Xresources

---
+++ xresources/Xresources.pp
@@ -0,0 +1,39 @@
+#foreach APP xterm, URxvt
+
+! Scrollbar
+APP.scrollBar: false
+APP.saveLines: 0
+
+APP*.font: xft:DejaVu Sans Mono:pixelsize=12 
+
+! Colors
+APP.color0:   Black
+APP.color1:   Red3
+APP.color2:   #51A006
+APP.color3:   Yellow3
+APP.color4:   #3669AB
+APP.color5:   Magenta3
+APP.color6:   Cyan3
+APP.color7:   AntiqueWhite
+APP.color8:   Grey25
+APP.color9:   Red
+APP.color10:  #90EB36
+APP.color11:  Yellow
+APP.color12:  #77A6D8
+APP.color13:  Magenta
+APP.color14:  Cyan
+APP.color15:  White
+
+APP.background: Black
+APP.foreground: White
+
+! Cursor
+APP.cursorColor: Red
+APP.cursorBlink: true
+APP.cursorUnderline: true
+
+! Title+Icon
+APP.title: Terminal
+APP.iconFile: /usr/share/icons/Clarity/scalable/apps/terminal.svg
+
+#endforeach
