d765378d9f7a17be4ba5168a531327a4c1225869
braph93@gmx.de <braph93@gmx.de>
2015-08-31 20:42:49 +0200

    ..

--- evilvte/config.h
+++ evilvte/config.h
@@ -47,24 +47,24 @@
 // #define COMMAND_SATURATION     TRUE  /* -sa option, saturation level       */
 // #define COMMAND_DOCK_MODE      TRUE  /* -d option, run as a dock           */
 #define    COMMAND_EXEC_PROGRAM   TRUE  /* -e option, execute program         */
-#define COMMAND_FULLSCREEN     TRUE  /* -f option, init in fullscreen mode */
-// #define COMMAND_FONT           TRUE  /* -fn option, to assign font & size  */
-#define COMMAND_GEOMETRY       TRUE  /* -g +X+Y option, to assign geometry */
+#define    COMMAND_FULLSCREEN     TRUE  /* -f option, init in fullscreen mode */
+#define    COMMAND_FONT           TRUE  /* -fn option, to assign font & size  */
+#define    COMMAND_GEOMETRY       TRUE  /* -g +X+Y option, to assign geometry */
 // #define COMMAND_LOGIN_SHELL    TRUE  /* -ls option, to enable login shell  */
 // #define COMMAND_SET_TITLE      TRUE  /* -T or -title to set program title  */
-#define COMMAND_SHOW_HELP      TRUE  /* -h option, show help               */
+#define    COMMAND_SHOW_HELP      TRUE  /* -h option, show help               */
 #define    COMMAND_SHOW_OPTIONS   TRUE  /* -o option, show build-time options */
 #define    COMMAND_SHOW_VERSION   TRUE  /* -v option, show program version    */
 // #define COMMAND_TAB_NUMBERS    TRUE  /* -[2-9] options, to set tab numbers */
-#define CURSOR_BLINKS          TRUE
-#define CURSOR_COLOR           "red"
-#define CURSOR_SHAPE  BLOCK /* Options: BLOCK, IBEAM, UNDERLINE                  */
+#define    CURSOR_BLINKS          TRUE
+#define    CURSOR_COLOR           "red"
+#define    CURSOR_SHAPE  BLOCK /* Options: BLOCK, IBEAM, UNDERLINE                  */
 // #define DEFAULT_COMMAND        g_getenv("SHELL")
 // #define DEFAULT_DIRECTORY      g_get_current_dir()
 // #define DEFAULT_TERMINAL_SIZE  80x24
 // #define EXPORT_WINDOWID        TRUE
-// #define FONT                   "Monospace 12"
-// #define FONT_ANTI_ALIAS        TRUE
+#define    FONT                   "Hack 12" /* Monospace 12 */
+#define    FONT_ANTI_ALIAS        TRUE
 // #define FONT_ENABLE_BOLD_TEXT  TRUE
 // #define MOUSE_CTRL_SATURATION  TRUE  /* Scroll left or right to adjust it  */
 // #define MOUSE_CURSOR_AUTOHIDE  TRUE
