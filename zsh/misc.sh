a5f08b95ddc78ff3df9c86abb0f3fedccab3810f
Benjamin Abendroth <braph93@gmx.de>
2015-10-29 12:54:39 +0100

    zsh

--- zsh/misc.sh
+++ zsh/misc.sh
@@ -4,3 +4,5 @@ ulimit -u 2000
 # Disable history expansion ####################################################
 #   "set +o histexpand" does not work, we use +H instead
 set +H
+
+Sysd=/lib/systemd/systemd/system
