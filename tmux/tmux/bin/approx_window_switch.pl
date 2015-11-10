062f33f1aea9bb7c4a815f658a6e27829b24592b
Benjamin Abendroth <braph93@gmx.de>
2015-11-10 10:56:55 +0100

    tmux

--- tmux/tmux/bin/approx_window_switch.pl
+++ tmux/tmux/bin/approx_window_switch.pl
@@ -5,42 +5,41 @@ use warnings;
 #use Data::Dump qw(dump);
 
 sub tmux_select_window($) {
-   system("tmux", "select-window", "-t", shift);
+   system('tmux', 'select-window', '-t', shift);
 }
 
 sub tmux_approx_select_window($) {
    my $win = shift;
    my @windows = sort {$a <=> $b} split("\n", `tmux list-windows -F '#I'`);
 
-   # only one window, do nothing
-   return if ($#windows == 0);
+   # only one (or no?!) window, do nothing
+   return if ($#windows <= 0);
 
    if ($win <= $windows[0]) {
       return tmux_select_window($windows[0]);
    }
-
-   if ($win >= $windows[$#windows]) {
+   elsif ($win >= $windows[$#windows]) {
       return tmux_select_window($windows[$#windows]);
    }
 
    my $approx_left;
 
    for (@windows) {
-      $approx_left = $_;
+      # wanted window is available
+      return tmux_select_window($win) if ($win == $_);
 
-      if ($win == $_) {
-         return tmux_select_window($win);
-      }
-      elsif ($_ > $win) {
+      if ($_ > $win) {
+         # check if right or left window is closed to the wanted window
          if ($win - $approx_left < $_ - $win) {
             return tmux_select_window($approx_left);
-         } else {
+         }
+         else {
             return tmux_select_window($_);
          }
       }
-   }
 
-   die "I don't know why I reached here.";
+      $approx_left = $_;
+   }
 }
 
 if ( $#ARGV != 0 ) {
