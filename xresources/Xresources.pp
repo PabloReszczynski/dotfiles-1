db8ea15f9d8f52fbdc76a63c2dc5cdd58d1c584a
Benjamin Abendroth <braph93@gmx.de>
2015-12-20 19:01:41 +0100

    xresources

--- xresources/Xresources.pp
+++ xresources/Xresources.pp
@@ -1,22 +1,5 @@
-#> foreach TERM xterm,URxvt
-
-#> include COLOR_THEME
-
-TERM*utf8:      2
-TERM*eightBitInput: true
-TERM*VT100.geomentry: 120x50
-
-! Scrollbar
-TERM*scrollBar: false
-TERM*saveLines: 0
-
-!TERM*.font:    xft:DejaVu Sans Mono:pixelsize=12 
-!TERM*faceName:  Terminus:size=13
-TERM*faceName:  Hack:size=12
-
-! Background/Foreground
-TERM*background: #111111
-TERM*foreground: #EEEEEE
+! ======== General Terminal Settings ===========
+#> foreach TERM xterm, URxvt
 
 ! Cursor
 TERM*cursorColor: Red
@@ -25,8 +8,45 @@ TERM*cursorBlink: true
 ! Title
 TERM*title: Terminal
 
-#> endforeach
+! Fixes ALT-Key
+TERM*metaSendsEscape: true
 
+! Disable changing window title
+TERM*allowTitleOps: false
+
+! UTF-8 Stuff
+TERM*utf8:            2
+TERM*eightBitInput:   true
+
+! Geometry
+TERM*geomentry: 120x80
+
+! Scrollbar
+TERM*scrollBar: false
+TERM*saveLines: 0
+
+! Font
+TERM*faceName:  Hack:size=12
+!TERM*.font:    xft:DejaVu Sans Mono:pixelsize=12 
+
+! === Color Scheme ===
+! Import scheme from themes/
+#> include COLOR_THEME
+! Overwrite Background/Foreground
+TERM*background: #111111
+TERM*foreground: #EEEEEE
+! ====================
+
+#> endforeach
+! ============================================
+
+! === URxvt ====
+URxvt.font: xft:Hack:size=12
+URxvt.letterSpace: -1
+URxvt*buffered: false
+! ==============
+
+! === Font ===
 Xft.antialias: 1
 Xft.autohint: 0
 Xft.dpi: 96
@@ -35,4 +55,5 @@ Xft.hinting: 1
 Xft.hintstyle: hintslight
 Xft.lcdfilter: lcddefault
 Xft.rgba: rgb
+! ============
 
