08ed0c3c1eb28fa43a458bf8debf53fc076b98c2
braph <braph93@gmx.de>
2016-12-21 13:41:00 +0100

    xinitrc: changed SHELL macro to SHELL_NAME (SHELL caused problems somehow)

--- xinitrc/xinitrc.pp
+++ xinitrc/xinitrc.pp
@@ -1,7 +1,7 @@
-#!/bin/SHELL
+#!/bin/SHELL_NAME
 
-LogFile="/dev/null"
-LogFile="/tmp/xinitrc.$USER.$$.WINDOW_MANAGER.log"
+#> define LogFile "/dev/null"
+#> define LogFile "/tmp/xinitrc.$USER.$$.WINDOW_MANAGER.log"
 
 run() {
    "$@" &
@@ -15,17 +15,47 @@ has_run() {
    has "$1" && run "$@"
 }
 
-run_as_script() {
-   echo "#!/bin/SHELL" > "/tmp/$1"
-   cat >> "/tmp/$1"
-   chmod +x "/tmp/$1"
-   "/tmp/$1" &
-   rm "/tmp/$1"
-}
-
 {
    date; # date for for log file
    sleep 3; # race conditions???
+
+   # === initialize X11 stuff =============================
+
+   # load Xresources
+   [ -e ~/.Xresources ]  && has_run xrdb ~/.Xresources
+   
+   # Set background to 'nearly black' (hack for fucking flash player)
+   has_run xsetroot -solid '#000001'
+ 
+   # Repeat those commands, in case the keyboard/mouse gets unplugged
+   while :; do
+     # Adjust keyboard repeat time/delay
+     xset r rate 255 60
+
+     # Adjust mouse acceleration
+     xset m 4 4
+
+     # Use capslock as compose key
+     setxkbmap -option compose:caps
+
+     sleep 60
+   done &
+
+   # Modify keyboard layout
+   [ -e ~/.Xmodmap ]     && has_run xmodmap ~/.Xmodmap
+  
+   # Load additional keybindings
+   [ -e ~/.xbindkeysrc ] && has_run xbindkeys
+
+   # Set background image if available
+   has_run nitrogen --restore
+
+   # Set up our screensaver
+   has_run xscreensaver -no-splash
+   #xset dpms 0 0 $(( 60 * 10 ))
+   #xscreensaver-command -lock
+   # ======================================================
+
 
    # === dirs that we want to keep in /tmp ================
    USER_TMP_DIR="/tmp/home-temp-${USER}";
@@ -45,44 +75,8 @@ run_as_script() {
    # ======================================================
 
 
-   # === initialize X11 stuff =============================
-   # Set background to 'nearly black' (hack for fucking flash player)
-   has_run xsetroot -solid '#000001'
- 
-   # Repeat those commands, in case the keyboard/mouse gets unplugged
-   while :; do
-     # Adjust keyboard repeat time/delay
-     xset r rate 255 60
-
-     # Adjust mouse acceleration
-     xset m 4 4
-
-     # Use capslock as compose key
-     setxkbmap -option compose:caps
-
-     sleep 60
-   done &
-
-   # load Xresources
-   [ -e ~/.Xresources ]  && has_run xrdb ~/.Xresources
-   
-   # Modify keyboard layout
-   [ -e ~/.Xmodmap ]     && has_run xmodmap ~/.Xmodmap
-  
-   # Load additional keybindings
-   [ -e ~/.xbindkeysrc ] && has_run xbindkeys
-
-   # Set background image if available
-   has_run nitrogen --restore
-
-   # Set up our screensaver
-   has_run xscreensaver -no-splash
-   #xset dpms 0 0 $(( 60 * 10 ))
-   #xscreensaver-command -lock
-   # ======================================================
-
-
    # === Select terminal emulator =========================
+      # TODO: tmux has-session -t main, tmux-new-session -d background
    if has uxterm; then
       terminal_cmd='uxterm -fullscreen -e'
    elif has xterm; then
@@ -104,17 +98,18 @@ run_as_script() {
          #has ncmpcpp && tmux new-window -d ncmpcpp
       fi
    elif has zsh; then
-      terminal_init='zsh'
+      terminal_init='zsh';
    else
-      terminal_init='bash'
+      terminal_init='bash';
    fi
 
    # === Start terminal emulator ==========================
    run $terminal_cmd $terminal_init;
 
    # === Open browser =====================================
-   has_run dillo;
-   has_run chromium;
+   has_run dillo blog.fefe.de;
+   
+   has_run inox;
 
    # === Host based applications ==========================
 #> if "HOST" eq "pizwo"
@@ -124,13 +119,13 @@ run_as_script() {
 #> endif
 #
    # === Music Player Daemon ==============================
-   has_run mpd
+   has_run mpd;
 
    # === Our notification daemon ===
    has_run dunst;
 
    # === Remap Mousebuttons ===
-   has_run imwheel
+   has_run imwheel;
 
 } >"$LogFile" 2>&1 &
 
