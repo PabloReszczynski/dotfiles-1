aaee4d5151a0ddd3896d6507a1c6100418495edc
Benjamin Abendroth <braph93@gmx.de>
2017-11-23 16:10:20 -0500

    shells: added common.mk

---
+++ shells/common.mk
@@ -0,0 +1,21 @@
+TMUX_VERSION = $(shell if which tmux >/dev/null 2>/dev/null; then \
+						tmux -V | sed 's/tmux //'; \
+					else \
+						echo 0.0; \
+					fi )
+
+GEMPATH = $(shell if which gem >/dev/null 2>/dev/null; then \
+			 				gem env gempath; \
+				fi )
+
+PRESERVE_ROOT = $(shell if cp --help | grep -q -- --preserve-root; then \
+					 		echo --preserve-root; \
+						fi)
+
+LS_COLOR = $(shell if ls --help | grep -q -- --color; then \
+			  		echo --color=auto; \
+				else \
+					echo -G; \
+				fi)
+
+HAVE_NVIM = $(shell which nvim >/dev/null 2>/dev/null && echo 1 || echo 0)
